9677df3e8d43be07986e5705fe6794a9fa2520ed
Trinh <t.trinh.it@gmail.com>
2023-04-02 22:52:04 +0700

    Add main tab buttons

--- src/main/Tab.tsx
+++ src/main/Tab.tsx
@@ -1,18 +1,28 @@
 import { FunctionComponent } from 'react'
+import iconChat from '../assets/icons/icon-chat.png'
+import iconChatActive from '../assets/icons/icon-chat-active.png'
+import iconPeople from '../assets/icons/icon-people.png'
+import iconPeopleActive from '../assets/icons/icon-people-active.png'
 
 const Tab: FunctionComponent = () => {
   return (
     <div className="tab">
-      <div className="container">
-        <ul className="tab__list">
-          <li className="tab__item">
-            <button className="tab__button tab__button--active" type="button">Chat</button>
-          </li>
-          <li className="tab__item">
-            <button className="tab__button" type="button">People</button>
-          </li>
-        </ul>
-      </div>
+      <ul className="tab__list">
+        <li className="tab__item">
+          <button className="tab-button" type="button">
+            <img src={iconChat} height="30" alt="" hidden />
+            <img src={iconChatActive} height="30" alt="" />
+            <span className="tab-button__text tab-button__text--active">Chat</span>
+          </button>
+        </li>
+        <li className="tab__item">
+          <button className="tab-button" type="button">
+            <img src={iconPeople} height="30" alt="" />
+            <img src={iconPeopleActive} height="30" alt="" hidden />
+            <span className="tab-button__text">People</span>
+          </button>
+        </li>
+      </ul>
     </div>
   )
 }
