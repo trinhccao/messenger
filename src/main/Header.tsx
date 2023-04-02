e727595a49cd9bf83e83cd679940ac7908c48890
Trinh <t.trinh.it@gmail.com>
2023-04-03 00:13:23 +0700

    Update

--- src/main/Header.tsx
+++ src/main/Header.tsx
@@ -2,13 +2,12 @@ import { FunctionComponent } from 'react'
 import { TabIds } from '../configs/tab-config'
 
 interface HeaderProps {
-  hidden?: boolean
   activeTab: TabIds
 }
 
-const Header: FunctionComponent<HeaderProps> = ({ hidden, activeTab }) => {
+const Header: FunctionComponent<HeaderProps> = ({ activeTab }) => {
   return (
-    <div className="header" hidden={hidden}>
+    <div className="header">
       <div className="container">
         <h1 className="header__title">{activeTab}</h1>
       </div>
