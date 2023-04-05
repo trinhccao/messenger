2845a45bf952fb5d6b4d5dc21e2cfeddac9e0198
Trinh <t.trinh.it@gmail.com>
2023-04-05 19:50:22 +0700

    Integrate conversation with api

--- src/main/ConversationItem.tsx
+++ src/main/ConversationItem.tsx
@@ -1,6 +1,8 @@
-import { FunctionComponent, MouseEvent } from 'react'
+import { FunctionComponent, MouseEvent, useEffect, useState } from 'react'
 import AvatarLarge from './AvatarLarge'
 import { useNavigate } from 'react-router-dom'
+import { IUser } from '../interfaces/IUser'
+import axios from 'axios'
 
 interface ConversationItemProps {
   userId: string
@@ -10,17 +12,32 @@ const ConversationItem: FunctionComponent<ConversationItemProps> = (props) => {
   const navigate = useNavigate()
   const { userId } = props
   const path = `/chat/${userId}`
+  const [user, setUser] = useState<IUser>()
 
   const onClick = (e: MouseEvent) => {
     e.preventDefault()
     navigate(path)
   }
 
+  useEffect(() => {
+    const abortController = new AbortController()
+    axios
+      .get<IUser>(`/users/${userId}`, { signal: abortController.signal })
+      .then(({ data }) => setUser(data))
+    return () => {
+      abortController.abort()
+    }
+  }, [userId])
+
+  if (!user) {
+    return null
+  }
+
   return (
     <a className="conversation-link" href={path} onClick={onClick}>
-      <AvatarLarge image="/images/dummy-1.jpg" isOnline={true} />
+      <AvatarLarge image={user.avatar || ''} isOnline={true} />
       <div className="conversation-link__content">
-        <h3 className="heading-lv3">Phuong Anh</h3>
+        <h3 className="heading-lv3">{user.firstName} {user.lastName}</h3>
         <p className="conversation-link__message">This chat has ended.</p>
       </div>
     </a>
