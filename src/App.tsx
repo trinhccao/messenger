d5d4ddc36c95db6d8aed977df268f636710e523f
Trinh <t.trinh.it@gmail.com>
2023-04-05 16:12:45 +0700

    Add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import root from './router/root'
+import './configs/axios'
 
 const App: FunctionComponent = () => {
   return (
