2565d8df844558f5d0bffca98cacde30777345b4
Trinh <t.trinh.it@gmail.com>
2023-04-09 05:20:34 +0700

    Update ChatItem.tsx

--- src/features/chat/ChatItem.tsx
+++ src/features/chat/ChatItem.tsx
@@ -1,6 +1,7 @@
 import {
   FunctionComponent,
   MouseEvent,
+  useContext,
   useEffect,
   useState,
 } from 'react'
@@ -8,7 +9,9 @@ import AvatarLarge from '../../common/AvatarLarge'
 import { useNavigate } from 'react-router-dom'
 import { DataMessage } from '../../models/DataMessage'
 import axios from 'axios'
-import { DataThread } from '../../models/DataThread'
+import { DataThread, ThreadTypes } from '../../models/DataThread'
+import { DataUser } from '../../models/DataUser'
+import { AuthContext } from '../../contexts/AuthContext'
 
 interface ChatItemProps {
   threadId: string
@@ -19,31 +22,49 @@ const ChatItem: FunctionComponent<ChatItemProps> = (props) => {
   const { threadId, messages } = props
   const navigate = useNavigate()
   const [thread, setThread] = useState<DataThread>()
-  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState<DataUser>()
+  const userFullName = user ? `${user?.firstName} ${user?.lastName}` : ''
+  const { authInfo } = useContext(AuthContext)
+  const isDirect = thread?.type === ThreadTypes.Direct
+  const path = `/chat/${isDirect ? user?._id : thread?._id}`
 
   const onClick = (e: MouseEvent) => {
     e.preventDefault()
+    navigate(path)
   }
 
   useEffect(() => {
     const controller = new AbortController()
     axios
-      .get(`/threads/${threadId}`, { signal: controller.signal })
+      .get<DataThread>(`/threads/${threadId}`, { signal: controller.signal })
       .then(({ data }) => setThread(data))
-      .finally(() => setLoading(false))
     return () => controller.abort()
   }, [threadId])
 
+  useEffect(() => {
+    if (!isDirect) {
+      return
+    }
+    const controller = new AbortController()
+    const userId = thread.members.find((id) => id !== authInfo?.user._id)
+    axios
+      .get<DataUser>(`/users/${userId}`, { signal: controller.signal })
+      .then(({ data }) => setUser(data))
+    return () => controller.abort()
+  }, [threadId, authInfo?.user._id, thread?.members, isDirect])
+
   return (
-    <a className="conversation-link" href={'path'} onClick={onClick}>
+    <a className="conversation-link" href={path} onClick={onClick}>
       <AvatarLarge
-        image={'isDirect ? user?.avatar : thread.avatar'}
+        image={isDirect ? user?.avatar : thread?.avatar}
         isOnline={true}
       />
       <div className="conversation-link__content">
-        <h3 className="heading-lv3">{'isDirect ? userFullName : thread.name'}</h3>
+        <h3 className="heading-lv3">
+          {isDirect ? userFullName : thread?.name || ''}
+        </h3>
         <p className="conversation-link__message">
-          {'latestMessage?.content' || ''}
+          {messages.slice(-1)[0]?.content || ''}
         </p>
       </div>
     </a>
