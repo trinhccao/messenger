47b2b9c60ac5f5cd41c27e8265a866d7672ca32e
Trinh <t.trinh.it@gmail.com>
2023-04-09 17:20:37 +0700

    Add logout feature

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,15 +1,30 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useContext } from 'react'
 import { TabIds } from '../tab/Tab'
+import { AuthContext } from '../../contexts/AuthContext'
 
 interface HeaderProps {
   activeTab: TabIds
 }
 
 const Header: FunctionComponent<HeaderProps> = ({ activeTab }) => {
+  const { setAuthInfo } = useContext(AuthContext)
+
+  const onLogout = () => {
+    localStorage.removeItem('authInfo')
+    setAuthInfo?.(undefined)
+  }
+
   return (
     <div className="header">
       <div className="container">
         <h1 className="header__title">{activeTab}</h1>
+        <button
+          className="header__button-logout"
+          type="button"
+          onClick={onLogout}
+        >
+          Logout
+        </button>
       </div>
     </div>
   )
