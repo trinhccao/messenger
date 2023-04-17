34867215f1cb5d9fe4caf815fd8ccae70f484c94
Trinh <t.trinh.it@gmail.com>
2023-04-18 02:09:00 +0700

    Implement online dot feature

--- src/features/home/SearchItem.tsx
+++ src/features/home/SearchItem.tsx
@@ -2,6 +2,8 @@ import { FunctionComponent, MouseEvent } from 'react'
 import AvatarSmall from './AvatarSmall'
 import { useNavigate } from 'react-router-dom'
 import { DataUser } from '../../types/DataUser'
+import { useAppSelector } from '../../redux/hooks'
+import { selectSocket } from '../../redux-slices/socket-slice'
 
 interface SearchItemProps {
   user: DataUser
@@ -11,6 +13,8 @@ const SearchItem: FunctionComponent<SearchItemProps> = ({ user }) => {
   const navigate = useNavigate()
   const path = `/chat/${user._id}`
   const fullName = `${user.firstName} ${user.lastName}`
+  const socket = useAppSelector(selectSocket)
+  const isOnline = socket.clientIds.some((id) => id === user._id)
 
   const onClick = (e: MouseEvent) => {
     e.preventDefault()
@@ -19,7 +23,7 @@ const SearchItem: FunctionComponent<SearchItemProps> = ({ user }) => {
 
   return (
     <a className="conversations__item" href={path} onClick={onClick}>
-      <AvatarSmall image={user.avatar} isOnline={true} />
+      <AvatarSmall image={user.avatar} isOnline={isOnline} />
       <div className="conversations__item-content">
         <span className="heading-lv3">{fullName}</span>
       </div>
